0d4a4f47d07878f5e900c1c7dca3e2dfea57519e
rameng0d <justin.bradshaw1203@gmail.com>
2024-05-12 20:23:39 -0400

    chore: Update dependencies in cnclient and add profiling to main.rs

--- CassandraNet/cncommon/src/profile/cnprofile.rs
+++ CassandraNet/cncommon/src/profile/cnprofile.rs
@@ -2,40 +2,51 @@ use superluminal_perf::*;
 
 pub struct CnProfile;
 
+// there might be a better way to do this, but htis is likely flexible enough
+pub trait Profiler {
+    /// Start a new event with the given name
+    fn start_event(event_name: &'static str);
+    /// End the current event
+    fn end_event();
+    /// Set the name of the current thread
+    fn set_thread_name(thread_name: &str);
+    /// Start a new event with the given name and data
+    fn start_event_with_data(event_name: &'static str, data: &str, color: u32);
+    /// Unregister a fiber
+    fn unregister_fiber(in_fiber_id: u64);
+    /// Begin a fiber switch
+    fn begin_fiber_switch(in_current_fiber_id: u64, in_new_fiber_id: u64);
+    /// End a fiber switch
+    fn end_fiber_switch(in_fiber_id: u64);
+}
+
 /// Public interface for profiling with cassandra-net. This is a wrapper around the superluminal_perf crate. if you want to use a different profiler, you can implement this trait for that profiler.
-impl CnProfile {
-    pub fn start_event(&self, event_name: &str) {
-        // superluminal_perf::begin_event(event_name);
-        todo!()
+impl Profiler for CnProfile {
+    fn start_event(event_name: &'static str) {
+        begin_event(event_name);
     }
 
-    pub fn end_event(&self, event_name: &str) {
-        // superluminal_perf::end_event();
-        todo!()
+    fn end_event() {
+        end_event();
     }
 
-    pub fn set_thread_name(&self, thread_name: &str) {
-        // superluminal_perf::set_current_thread_name(thread_name);
-        todo!()
+    fn set_thread_name(thread_name: &str) {
+        set_current_thread_name(thread_name);
     }
 
-    pub fn start_event_with_data(&self, event_name: &str, data: &str, color: u32) {
-        // superluminal_perf::begin_event_with_data(event_name, data, color);
-        todo!()
+    fn start_event_with_data(event_name: &'static str, data: &str, color: u32) {
+        begin_event_with_data(event_name, data, color);
     }
 
-    pub fn unregister_fiber(in_fiber_id: u64) {
-        // superluminal_perf::unregister_fiber(in_fiber_id);
-        todo!()
+    fn unregister_fiber(in_fiber_id: u64) {
+        unregister_fiber(in_fiber_id);
     }
 
-    pub fn begin_fiber_switch(in_current_fiber_id: u64, in_new_fiber_id: u64) {
-        // superluminal_perf::begin_fiber_switch(in_current_fiber_id, in_new_fiber_id);
-        todo!()
+    fn begin_fiber_switch(in_current_fiber_id: u64, in_new_fiber_id: u64) {
+        begin_fiber_switch(in_current_fiber_id, in_new_fiber_id);
     }
 
-    pub fn end_fiber_switch(in_fiber_id: u64) {
-        // superluminal_perf::end_fiber_switch(in_fiber_id);
-        todo!()
+    fn end_fiber_switch(in_fiber_id: u64) {
+        end_fiber_switch(in_fiber_id);
     }
 }
